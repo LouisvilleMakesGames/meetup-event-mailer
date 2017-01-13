4414a69b2cd89f14e64bbdac250fefcadebba991
abezuska <abezuska@gmail.com>
2017-01-12 21:23:54 -0500

    refactor listing events

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 var ical = require('ical');
 
-ical.fromURL('https://www.meetup.com/GameDevLou/events/ical/', {}, function(err, data) {
+ical.fromURL('https://www.meetup.com/GameDevLou/events/ical/', {}, generateEmail);
+
+
+function generateEmail (err, data) {
   if (err) {
     console.error(err);
     return;
   }
-  for (var k in data){
-    if (data.hasOwnProperty(k)) {
-      var ev = data[k];
-      if (ev.type !== "VEVENT") {
-        continue;
-      }
 
-      var start = new Date(ev.start);
-      var end = new Date(ev.end);
+  var messages = objectValues(data).filter(isValidEvent).map(eventToMessage);
+  console.log(messages);
 
-      var message = [
-        ev.summary,
-        "from",
-        start.toLocaleDateString(),
-        start.toLocaleTimeString(),
-        "to",
-        end.toLocaleDateString(),
-        end.toLocaleTimeString()
-      ].join(" ");
+}
 
-      console.log(message);
+function objectValues(obj) {
+  return Object.keys(obj).map( (key) => obj[key] );
+}
 
-    };
-
+function isValidEvent (ev) {
+  if (ev.type !== "VEVENT") {
+    return false;
   }
-});
+  return true;
+}
+
+function eventToMessage (ev) {
+  var start = new Date(ev.start);
+  var end = new Date(ev.end);
+
+  var message = [
+    ev.summary,
+    "from",
+    start.toLocaleDateString(),
+    start.toLocaleTimeString(),
+    "to",
+    end.toLocaleDateString(),
+    end.toLocaleTimeString()
+  ].join(" ");
+
+  return message;
+}
